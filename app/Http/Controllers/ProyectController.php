946a9df4ae34ec466f770db1eae36eaecdf83f9b
Hassiel Monterrosas <hassiel-monterrosas@hotmailcom>
2021-03-08 16:21:38 -0600

    añadiendo proyectos

--- app/Http/Controllers/ProyectController.php
+++ app/Http/Controllers/ProyectController.php
@@ -40,21 +40,42 @@ class ProyectController extends Controller
 
     public function show(Proyect $proyect)
     {
-        //
+        $proyecto = Proyect::find($id);
+        return view('show')->with('proyecto', $proyecto);
     }
 
     public function edit(Proyect $proyect)
     {
-        //
+        $proyecto = Proyect::find($id);
+        return view('proyects.edit')->with('proyectos', $proyecto);
     }
 
     public function update(Request $request, Proyect $proyect)
     {
-        //
+        //MODO PRO
+        $proyecto = Proyect::find($id);
+
+        $proyecto->update([
+           'name' => $request->name,
+           'description' => $request->description,
+           'final_date' => $request->final_date,
+            'hex' => $request->hex,
+        ]);
+        //REGRESO A LA PÁGINA ANTERIOR DEL DETALLE DE TAREA
+        //return redirect()->route('tareas.show', $tarea->id);
+            if ($request->origen == 'edit'){
+                redirect()->route('proyects.show', $proyecto->id);
+            }else{
+                return redirect()->back();
+            }
     }
 
     public function destroy(Proyect $proyect)
     {
-        //
+        $proyecto = Proyect::find($id);
+
+        $proyecto->delete();
+
+        return redirect()->route('proyects.index');
     }
 }

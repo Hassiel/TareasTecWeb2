1bc4606c08a095d6ac96adc7f8ad599487e69b6d
Hassiel Monterrosas <hassiel-monterrosas@hotmailcom>
2021-03-08 17:11:10 -0600

    tarjetas

--- app/Models/Proyect.php
+++ app/Models/Proyect.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Proyect extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name','description','final_date', 'hex'
+    ];
 }
---
+++ resources/views/proyects/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid mb-4" >
+    <div class="row align-items-center">
+        <div class="col-md-8">
+            <div class="title-page px-4 py-5">
+                <h3 class="display-1">Noé Hassiel</h3>
+                <p class="lead">Notion 2.0 N00B</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Crear nuevo proyecto
+            </button>
+        </div>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTarea">Crear nuevo proyecto</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+       <form action="{{route('proyectos.store')}}" method="POST">
+            <div class="modal-body">
+                <!--campo de protección de formulario-->
+            {{ csrf_field() }}
+            <!--Campos de formulario-->
+            <div class="form-group mb-3">
+                <label for="">Nombre de tarea</label>
+                <input class="form-control"type="text" placeholder="Nombre de tarea" name="name">
+            </div>
+            <div class="form-group mb-3">
+                <label for="">Descripción</label>
+                <textarea class="form-control" name="description" id="" cols="30" rows="10"></textarea>
+            </div>   
+            <div class="form-group mb-3">
+                <label for="">Fecha de Tarea</label>
+                <input class="form-control" type="date" name="due_date">
+            </div>
+                <br>
+            <div class="form-group mb-3">
+                <label for="">Color de proyecto</label>
+                <input class="form-control" type="color" name="hex">
+                </select>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-outline-success">Guardar proyecto</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card text-white bg-dark">
+                <h5 class="card-header">Listado de proyectos</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Proyectos en curso</h5>
+                        <p class="card-text">Este es tu listado de proyectos</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+                        <div class="row">
+                        @foreach ($proyectos as $proyectos)
+                        <div class="col-md-4 mt-4">
+                            <div class="card text-white bg-dark">
+                                <div class="line-color" style="height: 5px; width: 100%;
+                                background-color:{{ $proyectos->hex }}">
+                                    <div class="card-body">
+                                        <h4>{{ $proyectos->name }}</h4>
+                                        <p>{{ $proyectos->description }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        </div>
+</div>                        
+                
+@endsection

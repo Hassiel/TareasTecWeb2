300372d96a643e1b07e4e9445d2f87260a99c304
Hassiel Monterrosas <hassiel-monterrosas@hotmailcom>
2021-03-18 16:38:36 -0600

    agregar tarea

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,7 +10,7 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name','description','due_date', 'area', 'state', 'note'
+        'name','description','due_date', 'area', 'state', 'note', 'proyect_id'
     ];
 
     public function proyect()
--- resources/views/proyects/index.blade.php
+++ resources/views/proyects/index.blade.php
@@ -70,16 +70,76 @@
         </div>
     </div>
                         <div class="row">
-                        @foreach ($proyectos as $proyectos)
+                        @foreach ($proyectos as $proyecto)
                         <div class="col-md-4 mt-4">
                             <div class="card text-white bg-dark">
                                 <div class="line-color" style="height: 5px; width: 100%;
-                                background-color:{{ $proyectos->hex }}">
+                                background-color:{{ $proyecto->hex }}">
                                     <div class="card-body">
-                                        <h4>{{ $proyectos->name }}</h4>
-                                        <p>{{ $proyectos->description }}</p>
+                                        <h4>{{ $proyecto->name }}</h4>
+                                        <p>{{ $proyecto->description }}</p>
                                     </div>
+
+                                    <div class="tareas">
+                                        <ul>
+                                            @foreach ($proyecto->tasks as $tarea)
+                                                <li><a class="text-white" href="{{ route('tareas.show', $tarea->id) }}">{{ $tarea->name }}</a></li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    <a type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#agregarTarea_{{ $proyecto->id }}">
+                                        AGREGAR TAREA</a>
                                 </div>
+                                <!-- Modal -->
+<div class="modal fade" id="agregarTarea_{{ $proyecto->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalTarea">Crear nueva tarea</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+       <form action="{{route('tareas.store')}}" method="POST">
+            <div class="modal-body">
+                <!--campo de protección de formulario-->
+            {{ csrf_field() }}
+            <!--Campos de formulario-->
+            <div class="form-group mb-3">
+                <label for="">Nombre de tarea</label>
+                <input class="form-control"type="text" placeholder="Nombre de tarea" name="name">
+            </div>
+            <div class="form-group mb-3">
+                <label for="">Descripción</label>
+                <textarea class="form-control" name="description" id="" cols="30" rows="10"></textarea>
+            </div>   
+            <div class="form-group mb-3">
+                <label for="">Fecha de Tarea</label>
+                <input class="form-control" type="date" name="due_date">
+            </div>
+                <br>
+            <div class="form-group mb-3">
+                <label for="">Materia</label>
+                <select class="form-control mb-3" name="area" id="">
+                    <option value="Tecnologia Web">Tecnología Web</option>
+                    <option value="ASE 3">ASE 3</option>
+                    <option value="Prácticas Profesionales">Prácticas Profesionales</option>
+                    <option value="Branding">Branding</option>
+                </select>
+            </div>
+            <div class="form-group mb-3">
+                    @foreach ($proyectos as $proyecto)
+                        <input type="hidden" value="{{ $proyecto->id }}" class="form-control" name="proyect_id">
+                    @endforeach
+                </select>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cerrar</button>
+                <button type="submit" class="btn btn-outline-success">Guardar tarea</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
                             </div>
                         </div>
                         @endforeach
